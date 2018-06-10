996385778eb6e5a841419ffe16d74196bd3649c2
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-06-10 16:59:29 -0400

    rename

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace _02_ASP_intro
+namespace AspNetCoreIntro
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace _02_ASP_intro
+namespace AspNetCoreIntro
 {
     public class Startup
     {
