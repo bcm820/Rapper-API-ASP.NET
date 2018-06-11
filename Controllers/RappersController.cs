a69a5cbfd396f09838ac13cd69b119dac0a44018
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-06-11 01:08:41 -0400

    demo API

---
+++ Controllers/RappersController.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Rappers;
+
+namespace AspNetCoreIntro.Controllers {
+
+  public class RappersController : Controller {
+
+    public List<Artist> rappers;
+    public List<Group> groups;
+    public RappersController() {
+      rappers = JsonToFile<Artist>.ReadJson();
+      groups = JsonToFile<Group>.ReadJson();
+    }
+
+    [Route("rappers/")]
+    public string Index() {
+      StringBuilder instructions = new StringBuilder();
+      instructions.Append("Welcome to the Rapper API!\n\n");
+      instructions.Append("To get rapper info:\n");
+      instructions.Append("* all\n");
+      instructions.Append("* byName/{Name}\n");
+      instructions.Append("* byRealName/{RealName}\n");
+      instructions.Append("* byHometown/{Hometown}\n");
+      instructions.Append("To get group info:\n");
+      instructions.Append("* byGroup\n");
+      instructions.Append("* byGroup/{GroupName}\n");
+      instructions.Append("* byGroup/artists=?(true/false)\n");
+      return instructions.ToString();
+    }
+
+    [Route("rappers/all")]
+    public JsonResult RapperList() => Json(rappers);
+
+    [Route("rappers/byName/{Name}")]
+    public JsonResult ByName(string Name) =>
+      Json(rappers.Where(r => r.ArtistName.Contains(Name)));
+
+    [Route("rappers/byRealName/{RealName}")]
+    public JsonResult ByRealName(string RealName) =>
+      Json(rappers.Where(r => r.RealName.Contains(RealName)));
+
+    [Route("rappers/byHometown/{Hometown}")]
+    public JsonResult ByHometown(string Hometown) =>
+      Json(rappers.Where(r => r.Hometown.Contains(Hometown)));
+
+    [Route("rappers/byGroup")]
+    public JsonResult GroupList() => Json(groups);
+
+    [Route("rappers/byGroup/{Name}")]
+    public JsonResult ByGroupName(string Name) =>
+      Json(groups.Where(g => g.GroupName.Contains(Name)));
+
+    [Route("rappers/byGroup/showRappers={show}")]
+    public JsonResult AllGroups(string show) {
+      if (show != "true") return GroupList();
+      var rappersGroup = rappers.GroupBy(r => r.GroupId);
+      foreach (var rG in rappersGroup) {
+        var idx = groups.FindIndex(g => g.Id == rG.Key);
+        if (idx > 0)
+          foreach (var r in rG)
+            groups[idx].Members.Add(r);
+      }
+      return Json(groups);
+    }
+  }
+}
---
+++ data/Schemas.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Rappers {
+
+  public class Group {
+    public int Id;
+    public string GroupName;
+    public List<Artist> Members;
+    public Group() {
+      Members = new List<Artist>();
+    }
+  }
+
+  public class Artist {
+    public string ArtistName;
+    public string RealName;
+    public int Age;
+    public string Hometown;
+    public int GroupId;
+    public Group Group;
+  }
+
+  public class JsonToFile<T> {
+    public static List<T> ReadJson() {
+      string filename = $"data/{typeof(T).Name}.json";
+      using (StreamReader file = File.OpenText(filename)) {
+        JsonSerializer serializer = new JsonSerializer();
+        return (List<T>)serializer.Deserialize(file, typeof(List<T>));
+      }
+    }
+  }
+
+}
