e11aa4298c53b3175815739714080fa5859636c2
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-06-13 00:09:12 -0400

    use js to interact with c# generated DOM els

---
+++ wwwroot/route.js
@@ -0,0 +1,21 @@
+function pushToURI(route) {
+  return event => {
+    event.preventDefault();
+    if (route.includes("{")) {
+      const paramStart = route.indexOf("{");
+      const param = route.substring(paramStart + 1, route.indexOf("}"));
+      const input = prompt(`Enter parameter: ${route.substring(paramStart)}`, "");
+      if (!input) return;
+      route = route.substring(0, paramStart).concat(input);
+    }
+    window.location = route;
+  }
+}
+
+if (performance.navigation.type == 2)
+  location.reload(true);
+
+const elements = document.getElementsByClassName("route");
+for (const element of elements) {
+  element.addEventListener("click", pushToURI(element.getAttribute('href')));
+};
