91d8b091c9bcc17322727e2f91871009177fce6c
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-06-14 13:50:33 -0400

    add appsettings for EF core and API calls

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreIntro.Models;
 
 namespace AspNetCoreIntro {
 
   public class Startup {
 
+    public IConfiguration Configuration { get; private set; }
+
+    // Connects appsettings.json config file as an object
+    public Startup(IHostingEnvironment env) {
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+      Configuration = builder.Build();
+    }
+
     // Use this method to add services to the container
     public void ConfigureServices(IServiceCollection services) {
-      services.AddSession();
       services.AddMvc();
+      services.AddSession();
+
+      // Add options config sections to services for injection
+      Action<DbContextOptionsBuilder> useMySqlAppSettings =
+        options => options.UseMySql(Configuration["DbConfig:ConsnectionString"]);
+      services.AddDbContext<Context>(useMySqlAppSettings);
+      services.Configure<ApiConfig>(Configuration.GetSection("ApiConfig"));
     }
 
     // Use this method to configure the HTTP request pipeline
